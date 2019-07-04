54a158522a2df822183edaa052791c1af07e0880
Franck Tonle <francktonlebruno@gmail.com>
2019-07-04 14:23:20 +0100

    Initialisation du module "gest-ventes-portlet" !

---
+++ modules/gest-ventes-portlet/src/main/java/com/gest/ventes/constants/GestionVentesPortletKeys.java
@@ -0,0 +1,11 @@
+package com.gest.ventes.constants;
+
+/**
+ * @author franck
+ */
+public class GestionVentesPortletKeys {
+
+	public static final String GESTIONVENTES =
+		"com_gest_ventes_GestionVentesPortlet";
+
+}
