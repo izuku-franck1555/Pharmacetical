76d399e5fe29240071386e966ce667b623f381bc
Franck Tonle <francktonlebruno@gmail.com>
2019-07-06 14:58:59 +0100

    Peite modification de ConnexionPortlet.java

--- modules/connexion-portlet/src/main/java/com/connexion/portlet/ConnexionPortlet.java
+++ modules/connexion-portlet/src/main/java/com/connexion/portlet/ConnexionPortlet.java
@@ -4,7 +4,14 @@ import com.connexion.constants.ConnexionPortletKeys;
 
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 
+import java.io.IOException;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
 import javax.portlet.Portlet;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -25,4 +32,34 @@ import org.osgi.service.component.annotations.Component;
 	service = Portlet.class
 )
 public class ConnexionPortlet extends MVCPortlet {
+	
+	@Override
+	public void init() throws PortletException {
+		// TODO Auto-generated method stub
+		super.init();
+	}
+	
+	@Override
+	public void render(RenderRequest renderRequest, RenderResponse renderResponse)
+			throws IOException, PortletException {
+		// TODO Auto-generated method stub
+		super.render(renderRequest, renderResponse);
+	}
+	
+	@Override
+	public void processAction(ActionRequest actionRequest, ActionResponse actionResponse)
+			throws IOException, PortletException {
+		// TODO Auto-generated method stub
+		super.processAction(actionRequest, actionResponse);
+	}
+	
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		super.destroy();
+	}
+
 }
+
+
+
